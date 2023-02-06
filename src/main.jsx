b794c563a7aff1a777bf1b3558a2ac59fa5d6e1b
R.A Borja <raborja.dev@gmail.com>
2023-02-06 12:03:02 +0800

    starter files  clean up

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+// components
+/// card
+/// card title
+/// advice
+/// divider
+/// button

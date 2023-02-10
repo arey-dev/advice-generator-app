2175f241976632c1f74f315ba31ad9a675f2c676
R.A Borja <raborja.dev@gmail.com>
2023-02-10 14:13:06 +0800

    add responsiveness

--- src/components/divider/Divider.jsx
+++ src/components/divider/Divider.jsx
@@ -1,9 +1,25 @@
+import { useState, useEffect } from "react";
 import div from "./pattern-divider-desktop.svg";
+import divMb from "./pattern-divider-mobile.svg";
 
 export function Divider() {
+  const [viewportWidth, setViewportWidth] = useState(
+    document.documentElement.clientWidth
+  );
+
+  useEffect(() => {
+    const handleResize = () =>
+      setViewportWidth(document.documentElement.clientWidth);
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
+
   return (
     <div className="img-wrapper">
-      <img src={div} alt="" aria-hidden />
+      <img src={viewportWidth >= 375 ? div : divMb} alt="" aria-hidden />
     </div>
   );
 }
